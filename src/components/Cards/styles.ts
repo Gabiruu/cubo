8081a1ea0eb3374e013f16b6b694d2c558604bc6
Gabriel Alves <gabriel@gestaointeligentesoftware.com.br>
2022-09-21 15:20:37 -0300

    add pdf reader on proj

--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -3,8 +3,8 @@ import styled, { css } from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
   align-items: center;
+  justify-content: space-between;
   //padding: 89px 343px;
   padding: 0px 30px;
   gap: 43px;
