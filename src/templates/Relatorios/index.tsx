86011912f003c5f168aa9930978521e440156c44
Gabriel Alves <gabriel@gestaointeligentesoftware.com.br>
2022-09-22 17:46:35 -0300

    add pages relatorios and contrato

---
+++ src/templates/Relatorios/index.tsx
@@ -0,0 +1,35 @@
+import Container from 'components/Container'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
+import ContractPen from 'components/icons/ContractPen'
+import Contract from 'components/icons/Contract'
+import Cards from 'components/Cards'
+import { useProblems } from 'contexts/ProblemsContext'
+
+import * as S from './styles'
+
+const Contrato = () => {
+  const { prioritizedProblems } = useProblems()
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <S.Wrapper>
+          <S.Title>Contrato</S.Title>
+          <S.CardsWrapper>
+            <Cards label="Contratos">
+              <Contract />
+            </Cards>
+            <Cards label="Novo Contrato">
+              <ContractPen />
+            </Cards>
+          </S.CardsWrapper>
+        </S.Wrapper>
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default Contrato
